b4eaa76430c4e99717ef06e05daa61dc90be61f8
Ivaneide Protásio <ivaneide@neemu.com>
2018-08-22 08:53:03 -0400

    add support to categoryId, productId, tagId and url parameters
    feat(add formatterdTagsCategories function)
    
    feat: modify formatterdTagsCategories function
    
    feat: add formattedTags and formattedCategories fucntion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import { ajax } from '@linx-impulse/commons-js/http/ajax';
 import config from './config';
 
+function formattedTags(tags) {
+  return (tags || []).map(tag => (tag.id || tag.name));
+}
+
+function getParent(categories, item) {
+  return categories.find(category => (
+    Array.isArray(category.parents) && category.parents.indexOf(item.id) !== -1
+  ));
+}
+
+function formattedCategories(categories) {
+  // Filter wrong formatted
+  const filteredCategories = (categories || []).filter(category => category && category.id);
+
+  // Find the root node
+  let item = filteredCategories.find(category => !category.parents);
+  const ids = [];
+
+  while (typeof item === 'object') {
+    ids.push(item.id);
+    item = getParent(filteredCategories, item);
+  }
+  return ids;
+}
+
 export const BannerClient = {
   getRecommendations({
     page,
@@ -10,6 +35,10 @@ export const BannerClient = {
     userId,
     homologation,
     timeout,
+    categories,
+    product,
+    tags,
+    url,
   } = {}) {
     if (!deviceId) {
       return Promise.reject(new TypeError('deviceId is required to get banners'));
@@ -26,7 +55,18 @@ export const BannerClient = {
     return new Promise((resolve, reject) => {
       ajax({
         url: `${config.server.baseUrl}${config.server.recommendationUrl}`,
-        params: { deviceId, page, source, showLayout, userId, homologation },
+        params: {
+          deviceId,
+          page,
+          source,
+          showLayout,
+          userId,
+          homologation,
+          categoryId: formattedCategories(categories),
+          productId: product,
+          tagId: formattedTags(tags),
+          url,
+        },
         success: resolve,
         error: reject,
         timeout,
