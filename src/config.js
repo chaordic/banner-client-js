069e71fd4c78f889cf3a94faa379f2c9fc70be50
Andrew Pacifico <andrewcpacifico@gmail.com>
2018-10-25 13:24:05 -0400

    refactor: deviceId is not required anymore

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
+  cookieName: {
+    deviceId: 'chaordic_browserId',
+  },
   server: {
     baseUrl: '//banner.chaordicsystems.com/banner/v1',
     recommendationUrl: '/recommendations',
